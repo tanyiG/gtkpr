f82788b78dd46b642b5e0ace26ba358f49559849
Tanyi Győző <tanyi.gyozo@gmail.com>
2026-05-04 10:45:39 +0200

    Remove sudo

--- scripts/test_knocking.sh
+++ scripts/test_knocking.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo -n "x" | sudo ip netns exec testns nc -u -w1 10.0.0.1 1111
-echo -n "x" | sudo ip netns exec testns nc -u -w1 10.0.0.1 2222
-echo -n "x" | sudo ip netns exec testns nc -u -w1 10.0.0.1 3333
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 1111
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 2222
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 3333
