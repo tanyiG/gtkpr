e91bbea71ec3092874b2620eee4e278b26b5638a
Tanyi Győző <tanyi.gyozo@gmail.com>
2026-05-04 13:06:59 +0200

    Test wrong sequence

---
+++ scripts/test_wrong_knocking.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 1111
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 3333
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 2222
+echo -n "x" | ip netns exec testns nc -u -w1 10.0.0.1 3333
